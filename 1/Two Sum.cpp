283a273f8bfacb831f380c614a7e1da22a61958c
azadrven-sys <aza.d.r.ven@gmail.com>
2026-04-26 21:20:26 +0300

    Refactor twoSum to use member variables
    Refactor twoSum function to use class member variables instead of local variables.

--- 1/Two Sum.cpp
+++ 1/Two Sum.cpp
@@ -1,17 +1,19 @@
 #include <vector>
 #include <unordered_map>
-using namespace std;
 class Solution {
+    unordered_map<int, int> seen;
+    int len;
+    int* arr;
+    int complement;
+    size_t (unordered_map<int, int>::*countFn)(const int&) const = &unordered_map<int, int>::count;
+    int x;
+    int i;
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> seen;
-	      int len = nums.size();
-        int* arr = nums.data();
-        int complement;
-        size_t (unordered_map<int, int>::*countFn)(const int&) const = &unordered_map<int, int>::count;
-	      int x;
-        for (int i = 0; i < len; i++) {
-	          x = *arr;
+	    len = nums.size();
+        arr = nums.data();
+        for (i = 0; i < len; i++) {
+	        x = *arr;
             complement = target - x;
             if ((seen.*countFn)(complement)) {
                 return {seen[complement], i};
